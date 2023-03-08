c76ba229518d2ab1c2e82fb716decc7dfda3028b
Maressa Dâmaris Lima Diógenes <50055001+MaressaD@users.noreply.github.com>
2023-03-08 14:50:24 -0300

    atualizando

--- README.js
+++ README.js
@@ -1,4 +1,6 @@
 /* 
+Em Unicalculo você é Carlos, um unicórnio professor de cálculo que descobrio que o que está assolando seus alunos e fazendo eles sucumbirem a reprovação são os malfeitosos números primos. Seu trabalho é destruílos atirando uma nota baixa neles. Mas cuidado! os outros números podem confundí-lo e você será penalizado..
+Unicalculo é um jogo desenvolvido em JacaScript utilizando a plataforma >>>js nome da plataforma<<<. Neste game vo
     Equipe: 
         Nome Joyce Barbosa Cabral- Subturma A (Líder) 
         Nome Maressa Dâmaris Lima Diógenes - Subturma A 
